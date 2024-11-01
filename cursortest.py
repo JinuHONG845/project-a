15b3fdf62522ab327da90d30f5550adb802648b3
jinuhong845 <aronos84@gmail.com>
2024-11-01 20:48:06 +0900

    test_11012048

--- cursortest.py
+++ cursortest.py
@@ -1,5 +1,15 @@
 import streamlit as st
+from datetime import datetime
 
 st.title("My Streamlit App")
 st.write("Hello IS IT OK OK OK?")
 st.write("연동테스트에 성공하였다")
+
+# 달력 추가
+selected_date = st.date_input(
+    "날짜를 선택하세요",
+    datetime.now(),  # 기본값으로 오늘 날짜 설정
+)
+
+# 선택된 날짜 표시
+st.write("선택하신 날짜:", selected_date)
